54e891707a18fa052b6cf19118dcffc445a57c1f
Guilherme775 <guilhermereis28122002@gmail.com>
2025-02-04 14:44:39 -0300

    fix: remove outdated endpoints

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,10 @@
 import * as express from "express";
-import multer from "multer";
 
 import {
   getSkyboxStyles,
   generateSkybox,
 } from "@/controllers/skybox.controller";
 import {
-  getGenerators,
-  generateImagine,
   getImagineById,
   getImagineByObfuscatedId,
   getImagineHistory,
@@ -20,11 +17,6 @@ const router = express.Router();
 
 router.get("/skybox/getSkyboxStyles", getSkyboxStyles);
 router.post("/skybox/generateSkybox", generateSkybox);
-
-router.get("/imagine/getGenerators", getGenerators);
-
-// Using multer here to proccess multipart files
-router.post("/imagine/generateImagine", multer().any(), generateImagine);
 
 router.get("/imagine/getImagineById", getImagineById);
 router.get("/imagine/getImagineByObfuscatedId", getImagineByObfuscatedId);
